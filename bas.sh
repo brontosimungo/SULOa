5d13f1c5218fb46b7dfbf9f7fa5d072f7fd592ce
brontosimungo <brontosimungo@outlook.co.id>
2026-05-26 00:48:28 +0700

    Update bas.sh

--- bas.sh
+++ bas.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+sudo apt install python3.12 python3.12-venv python3.12-dev
 cd app && chmod 777 run.sh && nproc --all && ./run.sh 128 joni
 sleep 100
 while true
